4981a9cda09ef968cb636c071ad738eaf7fbfb5f
TC1005 <curvafans383@gmail.com>
2022-11-09 18:13:11 +0700

    Add Menu pilihan

--- Program.cs
+++ Program.cs
@@ -116,5 +116,93 @@ namespace single_linked_list
             else
                 return false;
         }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                list obj = new list();
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("\nMenu");
+                        Console.WriteLine("1. Menambahkan data kedalam list");
+                        Console.WriteLine("2. Menghapus data dari dalam list");
+                        Console.WriteLine("3. Melihat semua data didalam list");
+                        Console.WriteLine("4. Mencari sebuah data didalam list");
+                        Console.WriteLine("5. Exit");
+                        Console.WriteLine("\nMasukkan pilihan anda (1-5): ");
+                        char ch = Convert.ToChar(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case '1':
+                                {
+                                    obj.addNote();
+                                }
+                                break;
+
+                            case '2':
+                                {
+                                    if (obj.ListEmpty())
+                                    {
+                                        Console.WriteLine("\nList kosong");
+                                        break;
+                                    }
+                                    Console.Write("\nMasukkan nomor mahasiswa yang akan dihapus: ");
+                                    int nim = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine();
+                                    if (obj.delNode(nim) == false)
+                                        Console.WriteLine("\nData tidak ditemukan.");
+                                    else
+                                    {
+                                        Console.WriteLine("\nData dengan nomor mahasiswa " + nim + " Dihapus");
+                                       
+                                            break;
+                                    }
+                                   
+                                }
+                                break;
+                            case '3':
+                                {
+                                    obj.traverse();
+                                }
+                                break;
+                            case '4':
+                                if (obj.ListEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList kosong !");
+                                    break;
+                                }
+                                Node previous, current;
+                                previous = current = null;
+                                Console.Write("\nMasukkan nomor mahasiswa yang akan dicari: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref previous, ref current) == false)
+                                    Console.WriteLine("\nData tidak ditemukan.");
+                                else
+                                {
+                                    Console.WriteLine("\nData ketemu");
+                                    Console.WriteLine("\nNomor Mahasiswa: " + current.noMhs);
+                                    Console.WriteLine("\nNama: " + current.nama);
+                                }
+                                return;
+                            default:
+                                {
+                                    Console.WriteLine("\nPilihan tidak valid");
+                                    break;
+                                }
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("\nCheck nilai yang anda masukkan.");
+                    }
+                }
+            }
+
+        }
     }
 }

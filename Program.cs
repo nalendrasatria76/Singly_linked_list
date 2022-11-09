4fcb971207dac03c702cedc7db53acd1be6b02fc
TC1005 <curvafans383@gmail.com>
2022-11-09 17:28:56 +0700

    Add Public Bool dan method untuk menghapus node tertentu

--- Program.cs
+++ Program.cs
@@ -62,5 +62,37 @@ namespace single_linked_list
             previous.next = nodeBaru;
         }
 
+
+        /*Method untuk menghapus node tertentu didalam list*/
+        public bool delNode(int nim)
+        {
+            Node previous, current;
+            previous = current = null;
+            /*check apakah node yang dimaksud ada didalam list atau tidak*/
+            if (Search(nim, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+                START = START.next;
+            return true;
+        }
+
+        /*Method untuk menge-check apakah node yang dimaksud ada didalam list atau tidak*/
+        public bool Search(int nim,ref Node previous, ref Node current)
+        {
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim != current.noMhs))
+            {
+                previous = current;
+                current = current.next;
+            }
+
+            if (current == null)
+                return (false);
+            else
+                return (true);
+        }
     }
 }
